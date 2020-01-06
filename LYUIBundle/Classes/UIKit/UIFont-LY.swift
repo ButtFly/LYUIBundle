53b9e869456f0d9ef02c20c3b15e873b1f0dfbea
ButtFly <315585758@qq.com>
2020-01-06 08:51:02 +0800

    添加了设计用到的字体

--- LYUIBundle/Classes/UIKit/UIFont-LY.swift
+++ LYUIBundle/Classes/UIKit/UIFont-LY.swift
@@ -11,6 +11,9 @@ import UIKit
 //MARK: - 设计常使用的字体
 extension UIFont {
     
+    //MARK: - <#name#>
+    /// <#name#>
+    
     public class func pingFangSC(ofSize size: CGFloat) -> UIFont {
         return UIFont(name: "PingFangSC-Regular", size: size) ?? UIFont.systemFont(ofSize: size)
     }
@@ -27,13 +30,99 @@ extension UIFont {
         return UIFont(name: "PingFangSC-Semibold", size: size) ?? UIFont.systemFont(ofSize: size)
     }
     
+    //MARK: - <#name#>
+    /// <#name#>
+    
     public class func dinCondensed(ofSize size: CGFloat) -> UIFont {
         return UIFont(name: "DINCondensed-Bold", size: size) ?? UIFont.systemFont(ofSize: size)
     }
+    
+    //MARK: - <#name#>
+    /// <#name#>
     
     public class func helveticaNeueCondensedBold(ofSize size: CGFloat) -> UIFont {
         return UIFont(name: "HelveticaNeue-CondensedBold", size: size) ?? UIFont.systemFont(ofSize: size)
     }
     
+    public class func helveticaNeueUltraLightItalic(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "HelveticaNeue-UltraLightItalic", size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+    
+    public class func helveticaNeueMedium(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "HelveticaNeue-Medium", size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+    
+    public class func helveticaNeueMediumItalic(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "HelveticaNeue-MediumItalic", size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+    
+    public class func helveticaNeueUltraLight(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "HelveticaNeue-UltraLight", size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+    
+    public class func helveticaNeueItalic(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "HelveticaNeue-Italic", size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+    
+    public class func helveticaNeueLight(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "HelveticaNeue-Light", size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+    
+    public class func helveticaNeueThinItalic(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "HelveticaNeue-ThinItalic", size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+    
+    public class func helveticaNeueThin(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "HelveticaNeue-Thin", size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+    
+    public class func helveticaNeueCondensedBlack(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "HelveticaNeue-CondensedBlack", size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+    
+    public class func helveticaNeue(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "HelveticaNeue", size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+    
+    public class func helveticaNeueBoldItalic(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "HelveticaNeue-BoldItalic", size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+    
+    public class func helveticaNeueBold(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "HelveticaNeue-Bold", size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+    
+    public class func helveticaNeueLightItalic(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "HelveticaNeue-LightItalic", size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+    
+    //MARK: - <#name#>
+    /// <#name#>
+    
+    public class func helveticaOblique(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Helvetica-Oblique", size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+    
+    public class func helveticaBoldOblique(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Helvetica-BoldOblique", size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+    
+    public class func helvetica(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Helvetica", size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+    
+    public class func helveticaLight(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Helvetica-Light", size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+    
+    public class func helveticaBold(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Helvetica-Bold", size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+    
+    public class func helveticaLightOblique(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Helvetica-LightOblique", size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+    
 }
+
 
